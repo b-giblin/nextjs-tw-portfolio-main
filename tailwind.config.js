7c6ee98ccdd75ef732f3b83432dccc23ad661be8
Brian <bgiblin893@gmail.com>
2023-01-21 14:35:23 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: [
+
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {fontFamily:{
+      burtons:'burtons'
+    }},
+  },
+  plugins: [],
+}
